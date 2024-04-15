3e3874323f109ac7169459e7d049d0188a476da0
Aman Prajapat <reviewbad0101@gmail.com>
2024-04-15 23:27:17 +0530

    Page 1 Done

--- script.js
+++ script.js
@@ -1,47 +1,71 @@
-var tl = gsap.timeline();
+function loadingAnimation() {
+  var tl = gsap.timeline();
 
-tl.from(".line h1", {
+  tl.from(".line h1", {
     y: 150,
     stagger: 0.25,
     duration: 0.6,
     delay: 0.5,
-});
+  });
 
-tl.from("#line1-part1", {
+  tl.from("#line1-part1", {
     opacity: 0,
     onStart: function () {
-        var h5timer = document.querySelector("#line1-part1 h5");
-        var grow = 0;
-        setInterval(function () {
-            if (grow <= 100) {
-                h5timer.innerHTML = grow++;
-            }
-            else {
-                grow = 100;
-            }
-        }, 33);
-    }
-});
+      var h5timer = document.querySelector("#line1-part1 h5");
+      var grow = 0;
+      var x = setInterval(function () {
+        if (grow <= 100) {
+          h5timer.innerHTML = grow++;
+        } else {
+          clearInterval(x);
+        }
+      }, 33);
+    },
+  });
 
-tl.to(".line h2",{
-    animationName:'anime',
-    opacity:'1',
-})
+  tl.to(".line h2", {
+    animationName: "anime",
+    opacity: "1",
+  });
 
-tl.to("#loader", {
+  tl.to("#loader", {
     opacity: 0,
-    duration: .2,
+    duration: 0.2,
     delay: 3,
-});
+  });
 
-tl.from("#page1",{
-    y:1600,
-    delay:.2,
-    duration:.5,
+  tl.from("#page1", {
+    y: 1600,
+    delay: 0.2,
+    duration: 0.5,
+    opacity: 0,
+    ease: Power4,
+  });
+
+  tl.to("#loader", {
+    display: "none",
+  });
+
+  tl.from("#nav",{
     opacity:0,
-    ease:Power4,
-})
+  })
 
-tl.to("#loader", {
-    display:'none',
-})
+  tl.from(".hero *",{
+    y:100,
+    stagger:0.2,
+  })
+}
+
+function cursorAnimation(){
+  document.addEventListener("mousemove", function (dets) {
+    gsap.to("#crsr", {
+      left: dets.pageX,
+      top: dets.pageY,
+    });
+  });
+  
+  Shery.makeMagnet("#nav-part2 h4");
+}
+
+loadingAnimation();
+cursorAnimation();
